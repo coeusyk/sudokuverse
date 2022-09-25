b23b849972f8fb24ec911108a62d3e265e4c8589
coeusyk <yash.karecha@outlook.com>
2022-09-25 21:55:36 +0530

    Completed Feature -> Sudoku Grid

--- game/ui/gameplay/gameplay.py
+++ game/ui/gameplay/gameplay.py
@@ -6,6 +6,6 @@ from game.core.constants import CELL_ATTRIBUTES
 gameplay_blueprint = Blueprint("gameplay_blueprint", __name__, template_folder="templates", static_folder="static", static_url_path="/ui/gameplay")
 
 
-@gameplay_blueprint.route("/game")
+@gameplay_blueprint.route("/gameplay")
 def gameplay_window():
     return render_template("grid.html", cell_attributes=CELL_ATTRIBUTES)
